78e107614769369ddb620a2fdfea98e88fcde5ff
hannah20-031 <97353922+hannah20-031@users.noreply.github.com>
2022-01-08 16:31:52 +0000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,15 @@
+def show_the_heart():
+    basic.show_icon(IconNames.HEART,400)
+    basic.clear_screen()
+    basic.pause(200)
+    basic.show_icon(IconNames.SMALL_HEART,140)
+    basic.show_icon(IconNames.HEART,400)
+    basic.clear_screen()
+    basic.pause(200)
+    basic.show_icon(IconNames.SMALL_HEART,140)
+    basic.clear_screen()
+repeatheart = True
+show_the_heart()
+while (repeatheart):
+    show_the_heart()
+
---
+++ main.ts
@@ -0,0 +1,17 @@
+function show_the_heart() {
+    basic.showIcon(IconNames.Heart, 400)
+    basic.clearScreen()
+    basic.pause(200)
+    basic.showIcon(IconNames.SmallHeart, 140)
+    basic.showIcon(IconNames.Heart, 400)
+    basic.clearScreen()
+    basic.pause(200)
+    basic.showIcon(IconNames.SmallHeart, 140)
+    basic.clearScreen()
+}
+
+let repeatheart = true
+show_the_heart()
+while (repeatheart) {
+    show_the_heart()
+}
